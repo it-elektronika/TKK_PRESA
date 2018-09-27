9a6b3b775ca89b848559c901ad74d4623ba2e231
luckyluka <lu.kavcic@gmail.com>
2018-09-27 09:36:12 +0200

    scrolling y max made bigger

--- pages.c
+++ pages.c
@@ -249,7 +249,7 @@ void pageThree(int pageNum)  /* I/O STATUS */
     }
     
     up_button(1220, 120, &modY, 100, 0);
-    down_button(1220, 720, &modY, 100, -1300);
+    down_button(1220, 720, &modY, 100, -13000);
   }
   else if(page_stage[pageNum] == 2)
   {
